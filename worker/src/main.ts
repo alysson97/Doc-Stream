88166b3fc293578b7180bd8d1594d291aff34de2
alysson <alysson18.af@gmail.com>
2026-02-11 00:49:23 -0300

    feat: docker config adjusts & bug fix & .env example

--- worker/src/main.ts
+++ worker/src/main.ts
@@ -2,16 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
+const rabbitmqUrl = process.env.RABBITMQ_URL;
+
+if (!rabbitmqUrl) {
+  throw new Error('RABBITMQ_URL is not defined in environment variables');
+}
+
+const microserviceOptions: MicroserviceOptions = {
+  transport: Transport.RMQ,
+  options: {
+    urls: [rabbitmqUrl],
+    queue: 'docstream_queue',
+  },
+};
+
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqp://docstream-rabbitmq:5672'],
-        queue: 'docstream-rabbitmq',
-      },
-    },
+    microserviceOptions,
   );
   // await app.listen(process.env.PORT ?? 3000);
   await app.listen();
